acab3a0170e81ff1aed79039f8dcb0db7a042e9b
pratik.parmar <pratik.parmar@globalsysinfo.com>
2022-09-19 15:38:34 +0530

    [TRY] different things

--- src/components/metadata.tsx
+++ src/components/metadata.tsx
@@ -8,18 +8,20 @@ type props = {
 const Metadata = ({ title, description }: props) => {
   return (
     <Head>
+      <meta data-rh="true" name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1,maximum-scale=1"/>
       <title>{ title ? title : "SEO-Friendly with Next.js" }</title>
       <meta name="description" content={ description ? description : "SEO-Friendly with Next.js" } />
       <meta name="author" content="Gerardo Valencia" />
       <meta property="og:title" content={title ? title : "SEO-Friendly with Next.js"} />
       <meta property="og:type" content="website" />
       <meta property="og:description" content={description ? description : "SEO-Friendly with Next.js"} />
-      <meta property="og:image" content="https://img.lovepik.com/photo/40007/3158.jpg_wh300.jpg"></meta>
-      <meta property="image" content="https://img.lovepik.com/photo/40007/3158.jpg_wh300.jpg"></meta>
+      <meta property="og:image" content="https://img.lovepik.com/photo/40007/3158.jpg_wh300.jpg"/>
+      <meta data-rh="true" name="twitter:image:src" content="https://img.lovepik.com/photo/40007/3158.jpg_wh300.jpg"/>
+      <meta data-rh="true" name="twitter:card" content="summary_large_image"/>
+      <meta property="image" content="https://img.lovepik.com/photo/40007/3158.jpg_wh300.jpg"/>
       <meta name="twitter:title" content={title ? title : "SEO-Friendly with Next.js"} />
       <meta name="twitter:description" content={description ? description : "SEO-Friendly with Next.js"} />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
+      </Head>
   );
 }
 
