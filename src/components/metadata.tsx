f8f9f6166a5a9f0639474a0ccb5f4530919a2291
pratik.parmar <pratik.parmar@globalsysinfo.com>
2022-09-19 15:48:34 +0530

    [REMOVE] twitter

--- src/components/metadata.tsx
+++ src/components/metadata.tsx
@@ -17,9 +17,9 @@ const Metadata = ({ title, description, image }: props) => {
       <meta property="og:type" content="website" />
       <meta property="og:description" content={description ? description : "SEO-Friendly with Next.js"} />
       <meta property="og:image" content={image ? image : "https://img.lovepik.com/photo/40007/3158.jpg_wh300.jpg"}/>
-      <meta data-rh="true" name="twitter:image:src" content={image ? image : "https://img.lovepik.com/photo/40007/3158.jpg_wh300.jpg"}/>
+      {/* <meta data-rh="true" name="twitter:image:src" content={image ? image : "https://img.lovepik.com/photo/40007/3158.jpg_wh300.jpg"}/> */}
       <meta data-rh="true" name="twitter:card" content="summary_large_image"/>
-      <meta property="image" content={image ? image : "https://img.lovepik.com/photo/40007/3158.jpg_wh300.jpg"}/>
+      {/* <meta property="image" content={image ? image : "https://img.lovepik.com/photo/40007/3158.jpg_wh300.jpg"}/> */}
       <meta name="twitter:title" content={title ? title : "SEO-Friendly with Next.js"} />
       <meta name="twitter:description" content={description ? description : "SEO-Friendly with Next.js"} />
       </Head>
