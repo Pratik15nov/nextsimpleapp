cb38b7884c731ba899534fb0dc448bf8bae2b9dd
pratik.parmar <pratik.parmar@globalsysinfo.com>
2022-09-29 17:03:49 +0530

    [ADD] img

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import Image from 'next/image';
+
+import example1 from '../../public/example_1.jpeg';
+
 import Metadata from "./metadata";
 
 const Home = () => {
   return (
     <React.Fragment>
+      <Image
+        src={example1}
+        alt="Picture of the author"
+      />
       <Metadata
         title="SEO-Friendly: 🏡 Home"
         description="Trying how Next.js manage the SEO."
